09fba3b6f5a43355f9d06574f53d6ed9510a72c9
Tim Firman <tkfirman@gmail.com>
2018-02-15 13:31:52 -0800

    implements class exercise

--- ToDoList.Tests/ControllerTests/HomeControllerTest.cs
+++ ToDoList.Tests/ControllerTests/HomeControllerTest.cs
@@ -7,20 +7,31 @@ using ToDoList.Models;
 namespace ToDoList.Tests
 {
     [TestClass]
-    public class HomeControllerTest
+    public class ItemsControllerTest
     {
-      [TestMethod]
+        [TestMethod]
         public void Index_ReturnsCorrectView_True()
         {
-          //Arrange
-          HomeController controller = new HomeController();
+            //Arrange
+            ItemsController controller = new ItemsController();
 
-          //Act
-          IActionResult indexView = controller.Index();
-          ViewResult result = indexView as ViewResult;
+            //Act
+            IActionResult indexView = controller.Index();
+            ViewResult result = indexView as ViewResult;
 
-          //Assert
-          Assert.IsInstanceOfType(result, typeof(ViewResult));
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+        [TestMethod]
+        public void Index_HasCorrectModelType_ItemList()
+        {
+            //Arrange
+            ViewResult indexView = new ItemsController().Index() as ViewResult;
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(List<Item>));
         }
 
     }
--- ToDoList.Tests/ModelTests/ItemTest.cs
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -1,10 +1,12 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList;
+using System.Collections.Generic;
+using ToDoList.Models;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTest
+  public class ItemTest : IDisposable
   {
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
@@ -18,6 +20,27 @@ namespace ToDoList.Tests
 
       //Assert
       Assert.AreEqual(description, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    public void Dispose()
+    {
+      Item.ClearAll();
     }
   }
 }
